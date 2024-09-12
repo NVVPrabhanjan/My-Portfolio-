082629e2c0233c3bce4fcb28ac75316c6824a7e7
NVVPrabhanjan <nvvenkatprabhanjan@gmail.com>
2024-09-12 13:34:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,31 +15,35 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
+      <nav className="my-8 md:my-16 animate-fade-in">
+        <ul className="flex flex-col md:flex-row items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm md:text-base duration-500 text-zinc-500 hover:text-zinc-300"
             >
               {item.name}
             </Link>
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+
+      <div className="hidden md:block w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-left" />
+      
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      
+      <h1 className="py-3 px-1 z-10 text-3xl sm:text-4xl md:text-6xl lg:text-9xl text-transparent bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text duration-1000">
         Prabhanjan
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 mx-52 ">
+      <div className="hidden md:block w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-right" />
+
+      <div className="my-8 md:my-16 text-center animate-fade-in">
+        <h2 className="text-xs sm:text-sm md:text-base text-zinc-500 px-4 sm:px-8 md:px-20 lg:px-52">
           An Information Science student with a strong foundation in Java, data
           structures, algorithms and problem-solving. Skilled in HTML, CSS,
           JavaScript, NodeJS, SQL and MongoDB. Good at working in teams with
