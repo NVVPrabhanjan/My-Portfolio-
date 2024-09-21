a014eaa474334d331e0528e3a4b0eb63672b5a72
NVVPrabhanjan <nvvenkatprabhanjan@gmail.com>
2024-09-21 14:47:43 +0530

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import IconCloud from "@/app/components/magicui/icon-cloud";
 
+// Icon slugs array
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+// Navigation array
 const navigation = [
   { name: "About Me", href: "/aboutme" },
   { name: "Contact", href: "/contact" },
@@ -30,27 +66,19 @@ export default function Home() {
       </nav>
 
       <div className="hidden md:block w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-left" />
-      
+
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      
+
       <h1 className="py-3 px-1 z-10 text-3xl sm:text-4xl md:text-6xl lg:text-9xl text-transparent bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text duration-1000">
         Prabhanjan
       </h1>
 
       <div className="hidden md:block w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-right" />
-
-      <div className="my-8 md:my-16 text-center animate-fade-in">
-        <h2 className="text-xs sm:text-sm md:text-base text-zinc-500 px-4 sm:px-8 md:px-20 lg:px-52">
-          An Information Science student with a strong foundation in Java, data
-          structures, algorithms and problem-solving. Skilled in HTML, CSS,
-          JavaScript, NodeJS, SQL and MongoDB. Good at working in teams with
-          strong communication skills, and taking on new projects and practical
-          tasks.
-        </h2>
-      </div>
+      {/* Pass the required iconSlugs prop */}
+      <IconCloud iconSlugs={slugs} width="200px" />
     </div>
   );
 }
